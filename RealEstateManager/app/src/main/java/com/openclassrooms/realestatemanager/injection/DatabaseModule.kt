5ea987d2d3fc7887deb24c18cf0ca740c5750583
Gabrielle Freville <gfreville5@gmail.com>
2023-04-03 21:00:59 +0200

    Implement DatabaseModule

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/injection/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.openclassrooms.realestatemanager.injection
+
+import android.content.Context
+import androidx.room.Room
+import com.openclassrooms.realestatemanager.database.PropertyDao
+import com.openclassrooms.realestatemanager.database.RealEstateManagerDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext : Context) : RealEstateManagerDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            RealEstateManagerDatabase::class.java,
+            "RealEstateManagerDatabase"
+        ).build()
+    }
+
+    @Provides
+    fun providePropertyDao(realEstateManagerDatabase: RealEstateManagerDatabase) : PropertyDao {
+        return realEstateManagerDatabase.propertyDao()
+    }
+}
