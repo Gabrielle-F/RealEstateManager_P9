257279d0736938f0bfaa16630659008a4d100abd
Gabrielle Freville <gfreville5@gmail.com>
2023-04-04 20:20:52 +0200

    Remove view binding in MainActivity, correct Toolbar implementation, delete toolbar layout

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainActivity.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainActivity.java
@@ -1,37 +1,30 @@
-package com.openclassrooms.realestatemanager.ui;
+package com.openclassrooms.realestatemanager.ui.main;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.openclassrooms.realestatemanager.R;
-import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding;
-import com.openclassrooms.realestatemanager.databinding.ActivityMainToolbarBinding;
-import com.openclassrooms.realestatemanager.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        ActivityMainToolbarBinding toolbar = binding.activityMainToolbarInclude;
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        BottomAppBar bottomAppBar = binding.activityMainBottomAppBar;
+        BottomAppBar bottomAppBar = findViewById(R.id.activity_main_bottom_app_bar);
         bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
