52729796d0d026101fe2e4ea0ba7fc939ab02c82
Gabrielle Freville <gfreville5@gmail.com>
2023-04-04 12:40:34 +0200

    Add insertProperty method

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/database/PropertyDao.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/database/PropertyDao.kt
@@ -1,7 +1,13 @@
 package com.openclassrooms.realestatemanager.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.openclassrooms.realestatemanager.model.Property
 
 @Dao
 interface PropertyDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertProperty(property: Property)
 }
