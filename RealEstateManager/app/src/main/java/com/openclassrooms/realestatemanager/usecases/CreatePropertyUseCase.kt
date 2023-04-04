d797af1b48a24be514b4d670e60aca096f2d2d64
Gabrielle Freville <gfreville5@gmail.com>
2023-04-04 12:44:29 +0200

    Implement CreatePropertyUseCase

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/usecases/CreatePropertyUseCase.kt
@@ -0,0 +1,13 @@
+package com.openclassrooms.realestatemanager.usecases
+
+import com.openclassrooms.realestatemanager.model.Property
+import com.openclassrooms.realestatemanager.repository.PropertyRepository
+import javax.inject.Inject
+
+class CreatePropertyUseCase @Inject constructor(private val propertyRepository: PropertyRepository) {
+
+    fun createProperty(property: Property) {
+        propertyRepository.createProperty(property)
+    }
+
+}
