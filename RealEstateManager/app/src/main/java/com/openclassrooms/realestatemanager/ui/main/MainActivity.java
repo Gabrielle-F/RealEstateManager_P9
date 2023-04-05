dc4cf12faba7d3322c974f457c7f986485190f6c
Gabrielle Freville <gfreville5@gmail.com>
2023-04-05 17:23:09 +0200

    Manage MainActivity's FAB behavior

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainActivity.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainActivity.java
@@ -1,14 +1,18 @@
 package com.openclassrooms.realestatemanager.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.ui.addProperty.AddPropertyActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,17 +24,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = findViewById(R.id.activity_main_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddPropertyActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         BottomAppBar bottomAppBar = findViewById(R.id.activity_main_bottom_app_bar);
+        bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
         bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 int id = item.getItemId();
                 if(id == R.id.bottom_app_bar_edit_property) {
-                    //TODO start editPropertyActivity with the id of the selectedProperty
+                    //TODO start EditPropertyFragment with id of the selected property
                     return true;
                 }
                 return false;
